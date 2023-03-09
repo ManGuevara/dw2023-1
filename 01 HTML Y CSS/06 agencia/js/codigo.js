560146cfb290b214ccddd5164ebd38f8cea66e4d
Eduardo Arroyo <dj.shadowjeab@gmail.com>
2023-03-09 09:24:41 -0500

    menu

--- 01 HTML Y CSS/06 agencia/js/codigo.js
+++ 01 HTML Y CSS/06 agencia/js/codigo.js
@@ -2,8 +2,16 @@
 
 let nav = document.querySelector('.nav');
 // nav.classList.add('cambioNav');
+let menu = document.querySelector('.nav__contenedor__menu');
+let btn = document.querySelector('.nav__contenedor--btn');
 
 // console.log(scrollY);
+btn.addEventListener('click', function(){
+    // console.log('hiciste click');
+    // menu.classList.add('mostrarMenu');
+    menu.classList.toggle('mostrarMenu');
+})
+
 
 window.addEventListener('scroll', function(){
     // console.log('hiciste scroll', scrollY);
