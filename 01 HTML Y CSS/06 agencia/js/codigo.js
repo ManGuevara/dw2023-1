839773c447914be4f0b97771e9fff5ce1b513b0c
Eduardo Arroyo <dj.shadowjeab@gmail.com>
2023-03-08 22:40:24 -0500

    nav

---
+++ 01 HTML Y CSS/06 agencia/js/codigo.js
@@ -0,0 +1,15 @@
+// console.log('funcionaaaaaaaaaaaaa');
+
+let nav = document.querySelector('.nav');
+// nav.classList.add('cambioNav');
+
+// console.log(scrollY);
+
+window.addEventListener('scroll', function(){
+    console.log('hiciste scroll', scrollY);
+    if(scrollY > 0){
+        nav.classList.add('cambioNav');
+    } else{
+        nav.classList.remove('cambioNav');
+    }
+});
