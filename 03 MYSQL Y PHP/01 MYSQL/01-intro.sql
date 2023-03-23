f0cb81bd4ebc120c1170af68017f2fa4bfa134bf
Eduardo Arroyo <dj.shadowjeab@gmail.com>
2023-03-22 22:34:17 -0500

    my 2

--- 03 MYSQL Y PHP/01 MYSQL/01-intro.sql
+++ 03 MYSQL Y PHP/01 MYSQL/01-intro.sql
@@ -32,9 +32,20 @@ ALTER TABLE personas ADD COLUMN per_genero VARCHAR(15)
 
 ALTER TABLE personas DROP COLUMN per_genero
 
-ALTER TABLE personas ADD COLUMN per_genero VARCHAR(15) BEFORE per_apellido
+ALTER TABLE personas ADD COLUMN per_genero VARCHAR(15) AFTER per_apellido
 
 INSERT INTO personas (per_nombre, per_apellido, per_dni) VALUES 
     ('Jorge', "Salas", '11111111')
 
 SELECT * FROM personas
+
+INSERT INTO personas (per_nombre, per_apellido, per_genero, per_dni, per_fechaNac) VALUES 
+    ("Malena", 'Casas', 'f', '22222222', '2000-10-10'),
+    ("Pedro", 'Sifuentes', 'M', '33333333', '2010-12-12'),
+    ("Sofia", 'Rodriguez', 'F', '44444444', '2016-05-05')
+
+INSERT INTO personas (per_nombre, per_apellido, per_genero, per_dni, per_fechaNac) VALUES 
+    ('Peter', 'Castle', 'M', '5555555', '1980-02-02 15:00:00')
+
+-- 💥💥 CON CUIDADO
+DELETE FROM personas
